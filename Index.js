7c394300b787f31f6abe09d2b392ac7bdadb4b43
abubakkar11 <abubakkar995@gmail.com>
2022-10-25 11:10:47 -0700

    course details added

--- Index.js
+++ Index.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 5000;
+const  cors = require('cors')
 
+app.use(cors())
 const courses = require('./data/courses/courses.json');
 const courseDetails = require('./data/course-details/course-details.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/courses', (req, res) => {
+app.get('/catagories', (req, res) => {
   res.send(courses)
 })
-app.get('/courses/:id', (req, res) => {
+app.get('/All-courses', (req, res) => {
+  res.send(courseDetails)
+})
+app.get('/course-details/:id', (req, res) => {
     const id = req.params.id;
     const courseId = courseDetails.find(c => c.category_id === id)
   res.send(courseId)
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const courseId = courseDetails.find(c => c._id === id)
+   res.send(courseId)
 })
 
 app.listen(port, () => {
